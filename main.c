a1e97b936551a34bc4e5a2a97c5d954b241ab861
Emmanuel <89190695+Emmynuel2@users.noreply.github.com>
2022-07-13 02:42:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,11 +18,7 @@ int main(void)
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-<<<<<<< HEAD
-	addr = (void *)0x7ffe637541f0;
-=======
 	_printf("Length:[%d, %i]\n", len, len);
->>>>>>> 51228381a7c40c80ebde4da3b64fe93b524963ab
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
